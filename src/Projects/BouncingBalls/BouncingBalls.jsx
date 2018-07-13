d838241348df2c8eee6fd1dc2344d69a0eae6108
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-13 08:59:25 +0100

    made ball go crazy if you mouseover it

--- src/Projects/BouncingBalls/BouncingBalls.jsx
+++ src/Projects/BouncingBalls/BouncingBalls.jsx
@@ -13,31 +13,14 @@ class BouncingBalls extends Component {
     canvas.height = window.innerHeight;
     const c = canvas.getContext('2d');
 
-    // c.fillStyle = '#fa31b3';
-    // c.fillRect(100, 100, 100, 100);
-
-    // c.fillStyle = '#3333cc';
-    // c.fillRect(250, 100, 100, 100);
-
-    // // line
-    // c.beginPath();
-    // c.moveTo(50, 300);
-    // c.lineTo(100, 600);
-    // c.lineTo(100, 400);
-    // c.lineTo(50, 300);
-    // c.lineTo(100, 200);
-    // c.strokeStyle = '#fa34a3';
-    // c.stroke();
-
-    // // arc / circle
-    // for (let i = 0; i < 50; i += 1) {
-    //   const x = Math.random() * window.innerWidth;
-    //   const y = Math.random() * window.innerHeight;
-    //   c.beginPath();
-    //   c.arc(x, y, 30, 0, Math.PI * 2, false);
-    //   c.strokeStyle = 'blue';
-    //   c.stroke();
-    // }
+    const mouse = {
+      x: undefined,
+      y: undefined
+    };
+    window.addEventListener('mousemove', e => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+    });
 
     function Circle(x, y, dx, dy, radius) {
       this.x = x;
@@ -46,16 +29,16 @@ class BouncingBalls extends Component {
       this.dy = dy;
       this.radius = radius;
 
-      this.draw = () => {
+      this.draw = function() {
         c.beginPath();
-        c.arc(this.x, this.y, 30, 0, Math.PI * 2, false);
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = 'blue';
         c.stroke();
         c.fillStyle = '#ff5151';
         c.fill();
       };
 
-      this.update = () => {
+      this.update = function() {
         if (
           this.x + this.radius > window.innerWidth ||
           this.x - this.radius < 0
@@ -70,22 +53,29 @@ class BouncingBalls extends Component {
         }
         this.x += this.dx;
         this.y += this.dy;
+
+        // interactivity mouseover
+        console.log(this.x);
+        console.log(mouse.x);
+        if (mouse.x - this.x < 30) {
+          this.radius += 1;
+        }
+
         this.draw();
       };
     }
 
     const circles = [];
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < 1; i += 1) {
       const radius = 30;
       const x = Math.random() * (window.innerWidth - radius * 2) + radius;
       const y = Math.random() * (window.innerHeight - radius * 2) + radius;
       // velocity (how fast it moves)
-      const dx = (Math.random() - 0.5) * 20;
-      const dy = (Math.random() - 0.5) * 20;
+      const dx = (Math.random() - 0.5) * 3;
+      const dy = (Math.random() - 0.5) * 3;
       circles.push(new Circle(x, y, dx, dy, 30));
     }
 
-    console.log(circles);
     const animate = () => {
       requestAnimationFrame(animate);
       // // circle.draw();
