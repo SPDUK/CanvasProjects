d900d98a4d2394d476520ca0ea9a952cc10a6250
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-13 12:36:32 +0100

    made the circle go in a circle, ncie

--- src/Projects/Gravity/Gravity.jsx
+++ src/Projects/Gravity/Gravity.jsx
@@ -35,33 +35,41 @@ class Gravity extends Component {
     });
 
     // Objects
-    function Object(x, y, radius, color) {
+    function Particle(x, y, radius, color) {
       this.x = x;
       this.y = y;
       this.radius = radius;
       this.color = color;
+      this.radians = 0;
+      this.velocity = 0.05;
+
+      this.update = () => {
+        // move points over time
+        this.radians += this.velocity;
+        this.x = x + Math.cos(this.radians) * 100;
+        this.y = y + Math.sin(this.radians) * 100;
+        this.draw();
+      };
+      this.draw = () => {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+      };
     }
 
-    Object.prototype.update = function() {
-      this.draw();
-    };
-
-    Object.prototype.draw = function() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
-    };
-
     // Implementation
-    let objects;
+    let particles;
     function init() {
-      objects = [];
+      particles = [];
 
-      for (let i = 0; i < 400; i++) {
-        // objects.push();
+      for (let i = 0; i < 1; i += 1) {
+        particles.push(
+          new Particle(canvas.width / 2, canvas.height / 2, 5, 'blue')
+        );
       }
+      console.log(particles);
     }
 
     // Animation Loop
@@ -69,10 +77,24 @@ class Gravity extends Component {
       requestAnimationFrame(animate);
       c.clearRect(0, 0, canvas.width, canvas.height);
 
-      c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-      // objects.forEach(object => {
-      //  object.update();
-      // });
+      particles.forEach(particle => {
+        particle.update();
+      });
+    }
+
+    function randomIntFromRange(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function randomColor(colors) {
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    function distance(x1, y1, x2, y2) {
+      const xDist = x2 - x1;
+      const yDist = y2 - y1;
+
+      return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
     }
 
     init();
