62fbfc4b32173b7d9ef5341f9acf3f4083e1b559
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-24 20:48:21 +0100

    added in avisual sort using flexbox

--- src/Projects/VisualizedSort/SortItem/SortItem.jsx
+++ src/Projects/VisualizedSort/SortItem/SortItem.jsx
@@ -3,19 +3,31 @@ import React, { Component } from 'react';
 import './sortitem.css';
 
 // TODO: eslint seems to think this is a good idea even though it's not, reformat it later
-const arr = [8, 6, 10, 4, 5, 2, 7, 1, 9, 3];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default class SortItem extends Component {
-  test() {
+  componentDidMount() {
+    this.randomizeItems();
+  }
+  randomizeItems() {
     const sItems = document.querySelectorAll('.sort-item');
     for (let i = 0; i < sItems.length; i++) {
       sItems[i].style.order = Math.floor(Math.random() * sItems.length) + 1;
-      console.log(sItems[i]);
+    }
+  }
+
+  sort() {
+    const sItems = document.querySelectorAll('.sort-item');
+    for (let i = 0; i < sItems.length; i++) {
+      setTimeout(() => {
+        sItems[i].style.order = sItems[i].innerHTML;
+        sItems[i].style.background = 'rgb(142, 255, 127)';
+      }, 1000 * i);
     }
   }
   render() {
     return (
-      <ul onClick={this.test} className="sort container">
+      <ul onClick={this.sort} className="sort container">
         {arr.map(i => (
           <li key={i} className="sort-item">
             {i}
