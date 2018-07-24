7b9fbe58253cc3eef627af39681346ed81df3ae0
SPDUK <33531423+SPDUK@users.noreply.github.com>
2018-07-24 21:17:34 +0100

    moved css, added in buttons to sort & randomize numbers

--- src/Projects/VisualizedSort/SortItem/SortItem.jsx
+++ src/Projects/VisualizedSort/SortItem/SortItem.jsx
@@ -1,33 +1,66 @@
 import React, { Component } from 'react';
 
-import './sortitem.css';
-
-// TODO: eslint seems to think this is a good idea even though it's not, reformat it later
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  21,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
+  49,
+  50
+];
 
 export default class SortItem extends Component {
   componentDidMount() {
-    this.randomizeItems();
-  }
-  randomizeItems() {
-    const sItems = document.querySelectorAll('.sort-item');
-    for (let i = 0; i < sItems.length; i++) {
-      sItems[i].style.order = Math.floor(Math.random() * sItems.length) + 1;
-    }
+    this.props.randomizeItems();
   }
 
-  sort() {
-    const sItems = document.querySelectorAll('.sort-item');
-    for (let i = 0; i < sItems.length; i++) {
-      setTimeout(() => {
-        sItems[i].style.order = sItems[i].innerHTML;
-        sItems[i].style.background = 'rgb(142, 255, 127)';
-      }, 1000 * i);
-    }
-  }
   render() {
     return (
-      <ul onClick={this.sort} className="sort container">
+      <ul className="sort container">
         {arr.map(i => (
           <li key={i} className="sort-item">
             {i}
--- src/Projects/VisualizedSort/VisualizedSort.jsx
+++ src/Projects/VisualizedSort/VisualizedSort.jsx
@@ -6,13 +6,41 @@ import './VisualizedSort.css';
 import SortItem from './SortItem/SortItem';
 
 export default class VisualizedSort extends Component {
+  randomizeItems() {
+    const sItems = document.querySelectorAll('.sort-item');
+    for (let i = 0; i < sItems.length; i++) {
+      sItems[i].style.order = 0;
+      sItems[i].style.order = Math.floor(Math.random() * sItems.length) + 1;
+      sItems[i].style.background = 'rgb(180, 180, 180)';
+    }
+  }
+
+  // eslint-disable-next-line
+    sort() {
+    const sItems = document.querySelectorAll('.sort-item');
+    for (let i = 0; i < sItems.length; i++) {
+      setTimeout(() => {
+        sItems[i].style.order = sItems[i].innerHTML;
+        sItems[i].style.background = 'rgb(142, 255, 127)';
+      }, 300 * i);
+    }
+  }
+
   render() {
     return (
       <ReactAux>
         <Navbar />
         <div className="container">
           <div className="sort">
-            <SortItem />
+            <SortItem randomizeItems={this.randomizeItems} />
+          </div>
+          <div className="sort-buttons">
+            <div onClick={this.randomizeItems} className="button is-warning">
+              Randomize Numbers
+            </div>
+            <div onClick={this.sort} className="button is-success">
+              Sort Numbers
+            </div>
           </div>
         </div>
       </ReactAux>
